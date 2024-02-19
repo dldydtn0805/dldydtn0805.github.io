b02655b9726cae2d12d830a5bd6a1dfaafa0ca42
dldydtn0805 <ssafysoo95@gmail.com>
2024-02-19 15:31:08 +0900

    chore : chore

---
+++ blog/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  images: {
+    unoptimized: true, // 이미지 정상적으로 불러올 수 있도록함
+  },
+  compiler: {
+    styledComponents: true, // styled-components 사용 시 컴파일러에 추가
+  },
+};
+
+module.exports = nextConfig;
